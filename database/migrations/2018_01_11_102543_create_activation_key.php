6580e3c8ebc1bcc1c8eafcf391ffac2e2f548d29
Unknown <wege@hdsolutions.de>
2018-01-11 11:00:55 +0000

    Activation Mail

---
+++ database/migrations/2018_01_11_102543_create_activation_key.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateActivationKey extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('users', function(Blueprint $table){
+            $table->string('activation_key', 191)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
