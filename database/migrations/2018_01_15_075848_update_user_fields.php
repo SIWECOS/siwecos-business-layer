fee9693f40a52906d7c0575bf6a5f51899ae9ca9
Marcel Wege <wege@hdsolutions.de>
2018-01-15 09:33:50 +0100

    Added Cors Header

--- database/migrations/2018_01_15_075848_update_user_fields.php
+++ database/migrations/2018_01_15_075848_update_user_fields.php
@@ -14,17 +14,16 @@ class UpdateUserFields extends Migration
     public function up()
     {
         Schema::table('users', function(Blueprint $table){
-            $table->string('activation_key', 191)->nullable();
-            $table->string('address',191)->nullable();
-            $table->string('plz',191)->nullable();
-            $table->string('city',191)->nullable();
-            $table->string('phone',191)->nullable();
-            $table->string('org_name', 191)->nullable();
-            $table->string('org_industry', 191)->nullable();
-            $table->string('org_address',191)->nullable();
-            $table->string('org_plz',191)->nullable();
-            $table->string('org_city',191)->nullable();
-            $table->string('org_phone',191)->nullable();
+            $table->string('address',191)->nullable()->change();
+            $table->string('plz',191)->nullable()->change();
+            $table->string('city',191)->nullable()->change();
+            $table->string('phone',191)->nullable()->change();
+            $table->string('org_name', 191)->nullable()->change();
+            $table->string('org_industry', 191)->nullable()->change();
+            $table->string('org_address',191)->nullable()->change();
+            $table->string('org_plz',191)->nullable()->change();
+            $table->string('org_city',191)->nullable()->change();
+            $table->string('org_phone',191)->nullable()->change();
         });
     }
 
