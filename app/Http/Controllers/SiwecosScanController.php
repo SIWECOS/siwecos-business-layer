65b7d03ee6d3a90ec8cc872315a4f64fe7e8d002
David Jardin <d.jardin@djumla.de>
2018-03-01 16:34:03 +0100

    Output raw scoretype too to be able to hide useless scans

--- app/Http/Controllers/SiwecosScanController.php
+++ app/Http/Controllers/SiwecosScanController.php
@@ -108,6 +108,7 @@ class SiwecosScanController extends Controller {
 				$item['description'] = $this->buildDescription( $item['name'], $item['score'] );
 				$item['report']      = $this->buildReport( $item['name'], $item['score'] );
 				$item['scoreType']   = array_has( $item, 'scoreType' ) ? __( 'siwecos.SCORE_' . $item['scoreType'] ) : '';
+                $item['scoreTypeRaw']   = array_has( $item, 'scoreType' ) ? $item['scoreType'] : '';
 				$item['testDetails'] = collect( $item['testDetails'] );
 				$item['testDetails']->transform( function ( $item, $key ) {
 					$item['name'] = __( $item['placeholder'] );
