540d5d8191f3f58864ae19294a1868de6a30033d
David Jardin <d.jardin@djumla.de>
2018-03-11 11:30:58 +0100

    update reset requeist

--- app/Http/Controllers/SiwecosUserController.php
+++ app/Http/Controllers/SiwecosUserController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\CreateUserRequestCaptcha;
 use App\Http\Requests\ForgotPasswordRequest;
 use App\Http\Requests\GetTokenByUserRequest;
 use App\Http\Requests\LoginUserRequest;
+use App\Http\Requests\ProcessForgotPasswordRequest;
 use App\Http\Requests\UpdateUserCreditsRequest;
 use App\Http\Requests\UpdateUserRequest;
 use App\Notifications\activationmail;
