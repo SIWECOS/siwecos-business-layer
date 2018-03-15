64bb1aa4a69f574ed4e0523058d326f33f16d5e1
Stephan Hradek <stephan.hradek@eco.de>
2018-03-15 12:27:26 +0100

    markedown mail

--- resources/views/mail/forgotpassword.blade.php
+++ resources/views/mail/forgotpassword.blade.php
@@ -1,40 +1,47 @@
-<p>Hallo %emai%,</p>
+Hallo %emai%,
 
-<p>Wir haben eine Anfrage zum Zurücksetzen Ihres Passwortes für Ihren Account %email% erhalten.</p>
+Wir haben eine Anfrage zum Zurücksetzen Ihres Passwortes für Ihren Account %email% erhalten.
 
-<p>Sie können Ihr Passwort über den untenstehenden Link zurücksetzen:</p>
-<p><a href="{{$reseturl}}">Passwort zurücksetzen</a></p>
+Sie können Ihr Passwort über den untenstehenden Link zurücksetzen:
 
-<p>Wenn Sie dies nicht selbst angestoßen haben, sind keine weiteren Maßnahmen nötig.</p>
+<{{$reseturl}}>
 
-<p>Bitte antworten Sie nicht an diese Adresse.</p>
-<br>
-<p>Mit freundlichen Grüßen,</p>
-<p>Ihr SIWECOS Team</p>
-<br><br>
+Wenn Sie dies nicht selbst angestoßen haben, sind keine weiteren Maßnahmen nötig.
 
+Bitte antworten Sie nicht an diese Adresse.
 
-<p>eco - Verband der Internetwirtschaft e.V.</p>
-<p>Lichtstraße 43h</p>
-<p>50825 Köln, Deutschland</p>
-<br>
-<p>Impressum: https://siwecos.de/impressum/</p>
-<p>eco – Verband der Internetwirtschaft e.V.</p>
-<br>
-<p>Geschäftsführer: Harald A. Summa</p>
-<p>Vorstand: Oliver Süme (Vorsitzender), Klaus Landefeld (stv. Vorsitzender), Felix Höger, Prof. Dr. Norbert
-    Pohlmann</p>
-<p>Vereinsregister: Amtsgericht Köln, VR 14478</p>
-<br>
-<p>Sitz des Vereins: Köln</p>
-<br>
-<p>SIWECOS.de - Auf der sicheren Seite</p>
-<br>
-<p>SIWECOS ist ein Gemeinschaftsprojekt vom eco – Verband der Internetwirtschaft e. V. und der Ruhr-Universität Bochum
+Mit freundlichen Grüßen,
+
+Ihr SIWECOS Team
+
+eco - Verband der Internetwirtschaft e.V.
+
+Lichtstraße 43h
+
+50825 Köln, Deutschland
+
+Impressum: https://siwecos.de/impressum/
+
+eco – Verband der Internetwirtschaft e.V.
+
+Geschäftsführer: Harald A. Summa
+
+Vorstand: Oliver Süme (Vorsitzender), Klaus Landefeld (stv. Vorsitzender), Felix Höger, Prof. Dr. Norbert
+    Pohlmann
+
+Vereinsregister: Amtsgericht Köln, VR 14478
+
+Sitz des Vereins: Köln
+
+SIWECOS.de - Auf der sicheren Seite
+
+SIWECOS ist ein Gemeinschaftsprojekt vom eco – Verband der Internetwirtschaft e. V. und der Ruhr-Universität Bochum
     mit Unterstützung des CMS Garden e.V. und des Bochumer IT-Security Startups Hackmanit und steht für „Sichere
-    Webseiten und Content Management Systeme“.<br>
+    Webseiten und Content Management Systeme“.
+
     Das Projekt wird gefördert durch das Bundesministerium für Wirtschaft und Energie (BMWI) und hat zum Ziel die
-    Webseitensicherheit für kleine und mittelständische Unternehmen langfristig zu erhöhen.<br>
+    Webseitensicherheit für kleine und mittelständische Unternehmen langfristig zu erhöhen.
+    
     Siwecos bietet einen Webseitenscanner, der Sicherheitslücken zuverlässig aufdeckt, Filterregeln für Hosting
     Anbieter, das Webangriffe frühzeitig identifiziert, sowie Aufklärungs- und Hilfsangebote für kleine und
-    mittelständische Unternehmen beim Betrieb von Webseiten mit Content Management Systemen.</p>
+    mittelständische Unternehmen beim Betrieb von Webseiten mit Content Management Systemen.
