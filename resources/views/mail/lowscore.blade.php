567d81bc0c4d2b984f70ce78e25bcd7d13778617
tom1266 <thomas.bechem@eco.de>
2018-06-29 09:55:30 +0200

    Änderungen Mail-Templates

--- resources/views/mail/lowscore.blade.php
+++ resources/views/mail/lowscore.blade.php
@@ -1,33 +1,41 @@
-@component('mail::message')
-<p>Sehr {{{ $salutation_id === 1 ? "geehrter Herr" : "geehrte Frau" }}} {{ $last_name }},</p>
-
-<p>Mit dieser Mail informieren wir Sie über das aktuelle Scan Ergebnis für Ihre Webseite.</p>
-
-<p>Eine detaillierte Aufstellung der einzelnen Tests und den ausführlichen Sicherheitsbericht für Ihre Webseite finden Sie in der beigefügten PDF Datei.</p>
-
-
-<p>Mit freundlichen Grüßen,</p>
-<p>Ihr SIWECOS Team</p>
-@component('mail::footer')
-<p>eco - Verband der Internetwirtschaft e.V.</p>
-<p>Lichtstraße 43h</p>
-<p>50825 Köln, Deutschland</p>
-<br>
-<p>Impressum: https://siwecos.de/impressum/</p>
-<p>eco – Verband der Internetwirtschaft e.V.</p>
-<br>
-<p>Hauptgeschäftsführer: Harald A. Summa</p>
-<p>Geschäftsführer: Alexander Rabe</p>
-<p>Vorstand: Oliver Süme (Vorsitzender), Klaus Landefeld (stv. Vorsitzender), Felix Höger, Prof. Dr. Norbert Pohlmann</p>
-<p>Vereinsregister: Amtsgericht Köln, VR 14478</p>
-<br>
-<p>Sitz des Vereins: Köln</p>
-<br>
-<p>SIWECOS.de - Auf der sicheren Seite</p>
-<br>
-<p>SIWECOS ist ein Gemeinschaftsprojekt vom eco – Verband der Internetwirtschaft e. V. und der Ruhr-Universität Bochum mit Unterstützung des CMS Garden e.V. und des Bochumer IT-Security Startups Hackmanit und steht für „Sichere Webseiten und Content Management Systeme“.<br>
-Das Projekt wird gefördert durch das Bundesministerium für Wirtschaft und Energie (BMWI) und hat zum Ziel die Webseitensicherheit für kleine und mittelständische Unternehmen langfristig zu erhöhen.<br>
-Siwecos bietet einen Webseitenscanner, der Sicherheitslücken zuverlässig aufdeckt, Filterregeln für Hosting Anbieter, das Webangriffe frühzeitig identifiziert, sowie Aufklärungs- und Hilfsangebote für kleine und
-mittelständische Unternehmen beim Betrieb von Webseiten mit Content Management Systemen.</p>
+@component('mail::layout')
+@slot('header')
+@component('mail::header', ['url' => config('app.main_url') ])
+SIWECOS
 @endcomponent
+@endslot
+Sehr {{{ $salutation_id === 1 ? "geehrter Herr" : "geehrte Frau" }}} {{ $last_name }},
+
+Mit dieser Mail informieren wir Sie über das aktuelle Scan Ergebnis für Ihre Webseite.
+
+Eine detaillierte Aufstellung der einzelnen Tests und den ausführlichen Sicherheitsbericht für Ihre Webseite finden Sie in der beigefügten PDF Datei.
+
+
+Mit freundlichen Grüßen,
+
+Ihr SIWECOS Team
+@slot('footer')
+@component('mail::footer')
+eco - Verband der Internetwirtschaft e.V.<br/>
+Lichtstraße 43h<br/>
+50825 Köln, Deutschland
+
+Impressum: https://siwecos.de/impressum/<br/>
+eco – Verband der Internetwirtschaft e.V.
+
+Hauptgeschäftsführer: Harald A. Summa<br/>
+Geschäftsführer: Alexander Rabe<br/>
+Vorstand: Oliver Süme (Vorsitzender), Klaus Landefeld (stv. Vorsitzender), Felix Höger, Prof. Dr. Norbert Pohlmann<br/>
+Vereinsregister: Amtsgericht Köln, VR 14478
+
+Sitz des Vereins: Köln
+
+SIWECOS.de - Auf der sicheren Seite
+
+SIWECOS ist ein Gemeinschaftsprojekt vom eco – Verband der Internetwirtschaft e. V. und der Ruhr-Universität Bochum mit Unterstützung des CMS Garden e.V. und des Bochumer IT-Security Startups Hackmanit und steht für „Sichere Webseiten und Content Management Systeme“.<br/>
+Das Projekt wird gefördert durch das Bundesministerium für Wirtschaft und Energie (BMWI) und hat zum Ziel die Webseitensicherheit für kleine und mittelständische Unternehmen langfristig zu erhöhen.<br/>
+Siwecos bietet einen Webseitenscanner, der Sicherheitslücken zuverlässig aufdeckt, Filterregeln für Hosting Anbieter, das Webangriffe frühzeitig identifiziert, sowie Aufklärungs- und Hilfsangebote für kleine und
+mittelständische Unternehmen beim Betrieb von Webseiten mit Content Management Systemen.<br/>
+@endcomponent
+@endslot
 @endcomponent
--- resources/views/mail/registration.blade.php
+++ resources/views/mail/registration.blade.php
@@ -1,38 +1,46 @@
-@component('mail::message')
-<p>Sehr {{{ $salutation_id === 1 ? "geehrter Herr" : "geehrte Frau" }}} {{ $last_name }},</p>
+@component('mail::layout')
+@slot('header')
+@component('mail::header', ['url' => config('app.main_url') ])
+SIWECOS
+@endcomponent
+@endslot
+Sehr {{{ $salutation_id === 1 ? "geehrter Herr" : "geehrte Frau" }}} {{ $last_name }},
 
-<p>vielen Dank für Ihre Anmeldung bei Siwecos</p>
+vielen Dank für Ihre Anmeldung bei Siwecos
 
-<p>Um Ihren Benutzernamen zu bestätigen und Ihr Konto zu aktivieren, klicken Sie bitte auf untenstehenden Link</p>
+Um Ihren Benutzernamen zu bestätigen und Ihr Konto zu aktivieren, klicken Sie bitte auf untenstehenden Link
 
 @component('mail::button', [ 'url' => $activateurl, 'color' => 'red' ])
 Aktivierung
 @endcomponent
 
-<p>Bitte antworten Sie nicht an diese Adresse.</p>
-<br>
-<p>Mit freundlichen Grüßen,</p>
-<p>Ihr SIWECOS Team</p>
+Bitte antworten Sie nicht an diese Adresse
+
+Mit freundlichen Grüßen,
+
+Ihr SIWECOS Team
+@slot('footer')
 @component('mail::footer')
-<p>eco - Verband der Internetwirtschaft e.V.</p>
-<p>Lichtstraße 43h</p>
-<p>50825 Köln, Deutschland</p>
-<br>
-<p>Impressum: https://siwecos.de/impressum/</p>
-<p>eco – Verband der Internetwirtschaft e.V.</p>
-<br>
-<p>Hauptgeschäftsführer: Harald A. Summa</p>
-<p>Geschäftsführer: Alexander Rabe</p>
-<p>Vorstand: Oliver Süme (Vorsitzender), Klaus Landefeld (stv. Vorsitzender), Felix Höger, Prof. Dr. Norbert Pohlmann</p>
-<p>Vereinsregister: Amtsgericht Köln, VR 14478</p>
-<br>
-<p>Sitz des Vereins: Köln</p>
-<br>
-<p>SIWECOS.de - Auf der sicheren Seite</p>
-<br>
-<p>SIWECOS ist ein Gemeinschaftsprojekt vom eco – Verband der Internetwirtschaft e. V. und der Ruhr-Universität Bochum mit Unterstützung des CMS Garden e.V. und des Bochumer IT-Security Startups Hackmanit und steht für „Sichere Webseiten und Content Management Systeme“.<br>
-Das Projekt wird gefördert durch das Bundesministerium für Wirtschaft und Energie (BMWI) und hat zum Ziel die Webseitensicherheit für kleine und mittelständische Unternehmen langfristig zu erhöhen.<br>
+eco - Verband der Internetwirtschaft e.V.<br/>
+Lichtstraße 43h<br/>
+50825 Köln, Deutschland
+
+Impressum: https://siwecos.de/impressum/<br/>
+eco – Verband der Internetwirtschaft e.V.
+
+Hauptgeschäftsführer: Harald A. Summa<br/>
+Geschäftsführer: Alexander Rabe<br/>
+Vorstand: Oliver Süme (Vorsitzender), Klaus Landefeld (stv. Vorsitzender), Felix Höger, Prof. Dr. Norbert Pohlmann<br/>
+Vereinsregister: Amtsgericht Köln, VR 14478
+
+Sitz des Vereins: Köln
+
+SIWECOS.de - Auf der sicheren Seite
+
+SIWECOS ist ein Gemeinschaftsprojekt vom eco – Verband der Internetwirtschaft e. V. und der Ruhr-Universität Bochum mit Unterstützung des CMS Garden e.V. und des Bochumer IT-Security Startups Hackmanit und steht für „Sichere Webseiten und Content Management Systeme“.<br/>
+Das Projekt wird gefördert durch das Bundesministerium für Wirtschaft und Energie (BMWI) und hat zum Ziel die Webseitensicherheit für kleine und mittelständische Unternehmen langfristig zu erhöhen.<br/>
 Siwecos bietet einen Webseitenscanner, der Sicherheitslücken zuverlässig aufdeckt, Filterregeln für Hosting Anbieter, das Webangriffe frühzeitig identifiziert, sowie Aufklärungs- und Hilfsangebote für kleine und
-mittelständische Unternehmen beim Betrieb von Webseiten mit Content Management Systemen.</p>
-  @endcomponent
-  @endcomponent
+mittelständische Unternehmen beim Betrieb von Webseiten mit Content Management Systemen.<br/>
+@endcomponent
+@endslot
+@endcomponent
