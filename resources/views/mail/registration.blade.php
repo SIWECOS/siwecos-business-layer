7dbfbd1d4b1f63b3ce2b1955d22907ad926a76b8
Stephan Hradek <stephan.hradek@eco.de>
2018-11-12 14:43:45 +0100

    Correct mail spelling

--- resources/views/mail/registration.blade.php
+++ resources/views/mail/registration.blade.php
@@ -6,7 +6,7 @@ SIWECOS
 @endslot
 Sehr {{{ $salutation_id === 1 ? "geehrter Herr" : "geehrte Frau" }}} {{ $last_name }},
 
-vielen Dank für Ihre Anmeldung bei Siwecos
+vielen Dank für Ihre Anmeldung bei Siwecos.
 
 Um Ihren Benutzernamen zu bestätigen und Ihr Konto zu aktivieren, klicken Sie bitte auf untenstehenden Link
 
@@ -14,7 +14,7 @@ Um Ihren Benutzernamen zu bestätigen und Ihr Konto zu aktivieren, klicken Sie b
 Aktivierung
 @endcomponent
 
-Bitte antworten Sie nicht an diese Adresse
+Bitte antworten Sie nicht an diese Adresse.
 
 Mit freundlichen Grüßen,
 
@@ -39,7 +39,7 @@ SIWECOS.de - Auf der sicheren Seite
 
 SIWECOS ist ein Gemeinschaftsprojekt vom eco – Verband der Internetwirtschaft e. V. und der Ruhr-Universität Bochum mit Unterstützung des CMS Garden e.V. und des Bochumer IT-Security Startups Hackmanit und steht für „Sichere Webseiten und Content Management Systeme“.<br/>
 Das Projekt wird gefördert durch das Bundesministerium für Wirtschaft und Energie (BMWI) und hat zum Ziel die Webseitensicherheit für kleine und mittelständische Unternehmen langfristig zu erhöhen.<br/>
-Siwecos bietet einen Webseitenscanner, der Sicherheitslücken zuverlässig aufdeckt, Filterregeln für Hosting Anbieter, das Webangriffe frühzeitig identifiziert, sowie Aufklärungs- und Hilfsangebote für kleine und
+SIWECOS bietet einen Webseitenscanner, der Sicherheitslücken zuverlässig aufdeckt, Filterregeln für Hosting Anbieter, das Webangriffe frühzeitig identifiziert, sowie Aufklärungs- und Hilfsangebote für kleine und
 mittelständische Unternehmen beim Betrieb von Webseiten mit Content Management Systemen.<br/>
 @endcomponent
 @endslot
