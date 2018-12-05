c11974de3a5e6e7c03d40b066193be758067a3a3
Sascha Brendel <mail@lednerb.eu>
2018-12-05 18:44:17 +0100

    Installed google cloud storage dependencies and added seal resource.

--- config/app.php
+++ config/app.php
@@ -179,6 +179,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Superbalist\LaravelGoogleCloudStorage\GoogleCloudStorageServiceProvider::class,
     ],
 
     /*
