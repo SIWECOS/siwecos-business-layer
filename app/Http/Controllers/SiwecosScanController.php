d851501bbe8cb942471227f8cc26fac690a2a277
Sascha Brendel <mail@lednerb.eu>
2019-02-06 11:20:36 +0100

    Removed inline comment and opened issue #97

--- app/Http/Controllers/SiwecosScanController.php
+++ app/Http/Controllers/SiwecosScanController.php
@@ -439,15 +439,6 @@ class SiwecosScanController extends Controller
     {
         $minimumscore = env('NOTIFICATION_LOW_SCORE', 50);
         if ($totalScore < $minimumscore) {
-            /**
-             * TODO: Benachrichtungs-Logik ändern.
-             * - Diese Funktion ist zur Zeit nur für Testzwecke implementiert.
-             * - Es werden noch keine Mails an die Nutzer geschickt
-             * - Es wird ein Flag benötigt, der in staging etc. die Nachrichten nur an die in der
-             *   .env definierten Empfänger versendet
-             * - Andernfalls sollen die recipients => additionalRecipients sein und unabhängig
-             *   von einem Eintrag in der User-Tabelle verwendet werden können.
-             */
             $recipients = env('NOTIFICATION_LOW_SCORE_RECIPIENTS', '');
             foreach (preg_split("/[\s,]+/", $recipients) as $recipient) {
                 $siwecosUser = User::whereEmail($recipient)->first();
