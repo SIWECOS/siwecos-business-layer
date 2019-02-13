9e19d7ecbf8b2dffc4bbc45f77b73185537c543e
Marcel Wege <mwege@byte5.de>
2019-02-13 17:11:47 +0100

    Travis Fix

--- deploy-master.sh
+++ deploy-master.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
-docker push "$DOCKER_REPO"/$DOCKER_IMAGE:latest
+docker push "$DOCKER_REPO"/"$DOCKER_IMAGE":latest
 
 echo $GCLOUD_KEY | base64 --decode -i > ${HOME}/gcloud-service-key.json
 gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
