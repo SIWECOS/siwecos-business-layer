045f6dd9bf605e5660af537e0b52ea9e50cebf64
Marcel Wege <mwege@byte5.de>
2019-02-13 17:30:13 +0100

    Fix Travis Build

--- deploy-master.sh
+++ deploy-master.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
-docker tag "$DOCKER_REPO"/"$DOCKER_IMAGE":latest
+docker tag "$DOCKER_IMAGE" "$DOCKER_REPO"/"$DOCKER_IMAGE":latest
 docker push "$DOCKER_REPO"/"$DOCKER_IMAGE":latest
 
 echo $GCLOUD_KEY | base64 --decode -i > ${HOME}/gcloud-service-key.json
