2013bc12ee218614e7c0b0e96d3e834c4e395240
Meike Jung <1636619+hexabinaer@users.noreply.github.com>
2021-11-09 18:22:55 +0100

    Update email signature

--- resources/views/mail/forgotpassword.blade.php
+++ resources/views/mail/forgotpassword.blade.php
@@ -20,29 +20,18 @@ Bitte antworten Sie nicht an diese Adresse.
 
 Mit freundlichen Grüßen,
 
-Ihr SIWECOS Team
+Ihr SIWECOS-Team
 @slot('footer')
 @component('mail::footer')
-eco - Verband der Internetwirtschaft e.V.<br />
-Lichtstraße 43h<br />
-50825 Köln, Deutschland
+CMS Garden e. V., Osloer Str. 17, 13359 Berlin, Germany
 
-Impressum: https://siwecos.de/impressum/<br />
-eco – Verband der Internetwirtschaft e.V.
+Impressum: https://www.cms-garden.org/impressum/
 
-Hauptgeschäftsführer: Harald A. Summa<br />
-Geschäftsführer: Alexander Rabe<br />
-Vorstand: Oliver Süme (Vorsitzender), Klaus Landefeld (stv. Vorsitzender), Felix Höger, Prof. Dr. Norbert Pohlmann<br />
-Vereinsregister: Amtsgericht Köln, VR 14478
+Geschäftsführender Vorstand: Stephan Luckow (Vorsitzender), Meike Jung, Maik Derstappen, 
+Vereinsregister: Amtsgericht Berlin-Charlottenburg, VR 33004 B
 
-Sitz des Vereins: Köln
-
-SIWECOS.de - Auf der sicheren Seite
-
-SIWECOS ist ein Gemeinschaftsprojekt vom eco – Verband der Internetwirtschaft e. V. und der Ruhr-Universität Bochum mit Unterstützung des CMS Garden e.V. und des Bochumer IT-Security Startups Hackmanit und steht für „Sichere Webseiten und Content Management Systeme“.<br />
-Das Projekt wird gefördert durch das Bundesministerium für Wirtschaft und Energie (BMWI) und hat zum Ziel die Webseitensicherheit für kleine und mittelständische Unternehmen langfristig zu erhöhen.<br />
-SIWECOS bietet einen Webseitenscanner, der Sicherheitslücken zuverlässig aufdeckt, Filterregeln für Hosting Anbieter, das Webangriffe frühzeitig identifiziert, sowie Aufklärungs- und Hilfsangebote für kleine und
-mittelständische Unternehmen beim Betrieb von Webseiten mit Content Management Systemen.<br />
+SIWECOS steht für „Sichere Webseiten und Content-Management-Systeme“ und hilft Website-Betreiber*innen, Sicherheitslücken auf ihren Webseiten zu erkennen, zu verstehen und zu beheben. 
+Das Projekt wurde initial gefördert durch das Bundesministerium für Wirtschaft und Energie (BMWI). 
 @endcomponent
 @endslot
 @endcomponent
